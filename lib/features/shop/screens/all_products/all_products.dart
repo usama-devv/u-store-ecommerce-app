bbe5caf7585f63a48bf6f8b2178b0bd0ee87d582
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2024-08-23 20:56:47 +0500

    updated all products screen

--- lib/features/shop/screens/all_products/all_products.dart
+++ lib/features/shop/screens/all_products/all_products.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:u_store/common/widgets/appbar/appbar.dart';
+import 'package:u_store/common/widgets/layout/grid_layout.dart';
+import 'package:u_store/common/widgets/products/product_cards/product_card_vertical.dart';
 import 'package:u_store/utils/constants/sizes.dart';
 
 class AllProductsScreen extends StatelessWidget {
@@ -9,7 +11,8 @@ class AllProductsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const UAppBar(title: Text('Popular Products'), showBackArrow: true),
+      appBar:
+          const UAppBar(title: Text('Popular Products'), showBackArrow: true),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(USizes.defaultSpace),
@@ -20,9 +23,23 @@ class AllProductsScreen extends StatelessWidget {
                 decoration: const InputDecoration(
                   prefixIcon: Icon(Iconsax.sort),
                 ),
-                items: [],
                 onChanged: (value) {},
+                items: [
+                  'Name',
+                  'Higher Price',
+                  'Lower Price',
+                  'Sale',
+                  'Newest',
+                  'Popularity'
+                ]
+                    .map((option) =>
+                        DropdownMenuItem(value: option, child: Text(option)))
+                    .toList(),
               ),
+              const SizedBox(height: USizes.spaceBtwSections),
+
+              /// Products
+              UGridLayout(itemCount: 10, itemBuilder: (_, index) => const UProductCardVertical()),
             ],
           ),
         ),
