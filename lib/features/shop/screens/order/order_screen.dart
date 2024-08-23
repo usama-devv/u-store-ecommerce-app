a5832d5bf0f5c97057283a931f64fd4097db365c
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2024-08-23 16:21:16 +0500

    added order screen

---
+++ lib/features/shop/screens/order/order_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OrderScreen extends StatelessWidget {
+  const OrderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
