febfcc5edc09ae4c2593c404d45eaea6197f8928
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2024-08-27 09:51:49 +0500

    initial commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:u_store/utils/constants/colors.dart';
 import 'features/authentication/screens/onboarding/onboarding.dart';
 import 'utils/theme/theme.dart';
 
@@ -16,13 +15,7 @@ class App extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: UAppTheme.lightTheme,
       darkTheme: UAppTheme.darkTheme,
-      /// Show Loader or Circular Progress Indicator meanwhile authentication repository is deciding to show relevant screen
-      home: const Scaffold(
-        backgroundColor: UColors.primary,
-        body: Center(
-          child: CircularProgressIndicator(color: UColors.white),
-        ),
-      ),
+      home: const OnboardingScreen(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:u_store/firebase_options.dart';
 import 'app.dart';
 
-Future<void> main() async {
+void main() {
   /// Add widgets Binding
-  final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-
   /// Getx Local Storage
-  await GetStorage.init();
   /// Todo: Await Native Splash
   /// Initialize Firebase
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
-
   /// Todo: Initialize Authentication
 
   runApp(const App());
